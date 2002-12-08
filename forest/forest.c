02e954c866df6d0181dc2f91f3c73db9ca6e8f85
session <session>
2002-12-08 02:27:44 +0000

    removed irrelevent keyboard inputs

--- forest/forest.c
+++ forest/forest.c
@@ -14,7 +14,7 @@
  * tim cheadle
  * tcheadle@gmu.edu
  *
- * $Id: forest.c,v 1.2 2002-12-04 06:24:37 session Exp $
+ * $Id: forest.c,v 1.3 2002-12-08 02:27:44 session Exp $
  */
 
 #include <GL/gl.h>
@@ -24,58 +24,71 @@
 #include <stdio.h>
 #include <time.h>
 
-#define NUM_TREES 2000
+#define NUM_TREES 3000
 #define TEX_SCALE 50
 
-/* static const GLint NUM_TREES = 20; */
-
-static GLint spotlight_on = 1;
-static GLfloat spotlight_cutoff = 35.0;
-struct Tree {
+static struct Tree {
 	float x;
 	float y;
 	float z;
 	float r;
 } trees[NUM_TREES];
-	
+
+
+/* Textures */
+static GLuint texture, texture2; 
+
 
 /*
- * Initialize the window buffer
+ * Light properties
  */
-void init(void) 
-{
-	/* Add an ambient light with a grey color */
-	GLfloat light0_specular[]  = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat light0_shininess[] = { 100.0 };
-	GLfloat light0_position[]  = { 15.0, 60.0, 35.0, 1.0 };
-	GLfloat light0_diffuse[] = { 0.8, 0.6, 0.6, 1.0 };
-	GLfloat light0_ambient[]   = { 0.4, 0.4, 0.4, 1.0 };
+static GLfloat sun_x = 30.0;
+static GLfloat sun_y = 25.0;
+static GLfloat sun_z = 10.0;
+ 
+static GLfloat light0_specular[]  = { 1.0, 1.0, 1.0, 1.0 };
+static GLfloat light0_shininess[] = { 100.0 };
+static GLfloat light0_position[]  = { 30.0, 25.0, 10.0, 1.0 };
+static GLfloat light0_diffuse[] = { 0.8, 0.6, 0.6, 1.0 };
+static GLfloat light0_ambient[]   = { 0.4, 0.4, 0.4, 1.0 };
+static GLfloat light0_direction[] = { 0.0, -1.0, 0.0 };
 
-	glClearColor (0.0, 0.03, 0.05, 0.0);
-	glShadeModel (GL_SMOOTH);
 
-	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
-	glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
-	glLightfv(GL_LIGHT0, GL_SHININESS, light0_shininess);
+/*
+ * Materials properties
+ */
+static GLfloat no_mat[] = { 0.0, 0.0, 0.0, 1.0 };
+ 
+/* Ground properties */
+static GLfloat floor_diffuse[] = { 0.2, 0.5, 0.8, 1.0 };
+static GLfloat floor_ambient[] = { 1, 1, 1, 1.0 };
+static GLfloat floor_specular[] = { 0.2, 0.5, 0.8, 1.0 };
+static GLfloat high_shininess[] = { 100.0 };
+
+/* Trunk & branch properties */
+static GLfloat wood_diffuse[] = { 0.4, 0.8, 0.2, 1.0 };
+static GLfloat wood_ambient[] = { 0.4, 0.8, 0.2, 1.0 };
+static GLfloat wood_specular[] = { 0.5, 0.7, 0.1, 1.0 };
 	
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_DEPTH_TEST);
-	glEnable( GL_TEXTURE_2D );
+/* Sun sphere properties */
+static GLfloat sun_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+static GLfloat sun_ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+static GLfloat sun_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+static GLfloat sun_emission[] = { 0.3, 0.3, 0.3, 1.0 };
 
-	/* Seed the random table */
-	srand(time(NULL));
-	
+
+
+void generateTrees() {
 	/* Generate tree locations */
 	for(int i=0; i < NUM_TREES; i++) {
-		trees[i].x = (rand() % 1350)/10.0 - 100.0;
+		trees[i].x = (rand() % 1500)/10.0 - 100.0;
 		trees[i].y = (rand() % 100)/10.0 + 2.0;
-		trees[i].z = (rand() % 1350)/10.0 - 100.0;
+		trees[i].z = (rand() % 1500)/10.0 - 100.0;
 		trees[i].r = (rand() % 60)/100.0 + 0.1;
 	}
 }
+
+
 
 
 /* load a 256x256 RGB .RAW file as a texture */
@@ -133,43 +146,16 @@ GLuint LoadTextureRAW( const char * filename, int wrap )
 
 
 
-
-
-
-/*
- * Draw the room
- */
-void display(void)
-{
-	GLfloat no_mat[] = { 0.0, 0.0, 0.0, 1.0 };
-	GLuint texture, texture2; 
-	
-	/* Floor properties */
-	GLfloat floor_diffuse[] = { 0.2, 0.5, 0.8, 1.0 };
-	GLfloat floor_ambient[] = { 1, 1, 1, 1.0 };
-	GLfloat floor_specular[] = { 0.2, 0.5, 0.8, 1.0 };
-	GLfloat high_shininess[] = { 100.0 };
-
-	/* Trunk & branch properties */
-	GLfloat wood_diffuse[] = { 0.4, 0.8, 0.2, 1.0 };
-	GLfloat wood_ambient[] = { 0.4, 0.8, 0.2, 1.0 };
-	GLfloat wood_specular[] = { 0.5, 0.7, 0.1, 1.0 };
-	
-	GLfloat sun_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat sun_ambient[] = { 0.0, 0.0, 0.0, 1.0 };
-	GLfloat sun_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat sun_emission[] = { 1.0, 1.0, 1.0, 1.0 };
-	
-
-	/* Clear the output color buffer) */
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	/* Get texture */
+void loadTextures() {
 	texture = LoadTextureRAW("grass-tile.bmp", 1);
 	texture2 = LoadTextureRAW("gravel-tile.bmp", 1);
+}
+
+
+
+void drawGround() {
 	glBindTexture( GL_TEXTURE_2D, texture );
 
-	/* Floor */
 	glPushMatrix();
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, floor_ambient);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, floor_diffuse);
@@ -183,18 +169,21 @@ void display(void)
 		
 		glTexCoord2f(TEX_SCALE, 0);
 		glNormal3i(0, 1, 0);
-		glVertex3i(35, 0, -100);
+		glVertex3i(50, 0, -100);
 		
 		glTexCoord2f(0, 0);
 		glNormal3i(0, 1, 0);
-		glVertex3i(35, 0, 35);
+		glVertex3i(50, 0, 50);
 		
 		glTexCoord2f(0, TEX_SCALE);
 		glNormal3i(0, 1, 0);
-		glVertex3i(-100, 0, 35);
+		glVertex3i(-100, 0, 50);
 	glEnd();
 	glPopMatrix();
+}
 
+
+void drawTrees() {
 	glPushMatrix();
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, wood_ambient);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, wood_diffuse);
@@ -216,10 +205,14 @@ void display(void)
 		glPushMatrix();
 		glTranslatef(x, 0.0, z);
 		glRotatef(-90.0, 1.0, 0.0, 0.0);
-		glutSolidCone(r, y, 30, 10);
+		glutSolidCone(r, y, 10, 1);
 		glPopMatrix();
 	}
-	
+}
+
+
+
+void drawSun() {
 	GLUquadricObj *q = gluNewQuadric();
 	gluQuadricNormals(q, GL_SMOOTH);					// Generate Smooth Normals For The Quad
 	gluQuadricTexture(q, GL_TRUE);						// Enable Texture Coords For The Quad
@@ -232,13 +225,67 @@ void display(void)
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, sun_diffuse);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, sun_specular);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, high_shininess);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, no_mat);
-	glTranslatef(10.0, 10.0, 10.0);
-	gluSphere(q, 1.0, 60, 60); 
+	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, sun_emission);
+	glTranslatef(sun_x, sun_y + 5.0, sun_z);
+	gluSphere(q, 5.0, 60, 60); 
 	glPopMatrix();
+	
+	/* Reset light position */
+	light0_position[0] = sun_x;
+	light0_position[1] = sun_y;
+	light0_position[2] = sun_z;
 	
 	glDisable(GL_TEXTURE_GEN_S);						// Disable Sphere Mapping
 	glDisable(GL_TEXTURE_GEN_T);						// Disable Sphere Mapping
+}
+
+
+
+
+
+/*
+ * Initialize the window buffer
+ */
+void init(void) 
+{
+	/* Add an ambient light with a grey color */
+
+
+	glClearColor (0.0, 0.03, 0.05, 0.0);
+	glShadeModel (GL_SMOOTH);
+
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
+	glLightfv(GL_LIGHT0, GL_SHININESS, light0_shininess);
+	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light0_direction);
+	
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);
+	glEnable( GL_TEXTURE_2D );
+
+	/* Seed the random table */
+	srand(time(NULL));
+	
+	generateTrees();
+}
+
+
+/*
+ * Draw the room
+ */
+void display(void)
+{
+	/* Clear the output color buffer) */
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	loadTextures();
+	
+	drawGround();
+	drawTrees();
+	drawSun();
 	
 	glutSwapBuffers();
 	glDeleteTextures( 1, &texture );
@@ -253,8 +300,8 @@ void reshape(int w, int h)
 	glViewport (0, 0, (GLsizei) w, (GLsizei) h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(60.0, 1.0, 2.0, 400.0);
-	gluLookAt(35.0, 15.0, 25.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);	
+	gluPerspective(60.0, 1.5, 1.0, 400.0);
+	gluLookAt(35.0, 35.0, 35.0, 0.0, 20.0, 0.0, 0.0, 1.0, 0.0);	
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
@@ -266,31 +313,6 @@ void reshape(int w, int h)
 void keyboard(unsigned char key, int x, int y) 
 {
 	switch (key) {
-		case '-':
-			spotlight_cutoff -= 5.0;
-			glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, spotlight_cutoff);
-			glutPostRedisplay();
-			break;
-			
-		case '+':
-			spotlight_cutoff += 5.0;
-			glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, spotlight_cutoff);
-			glutPostRedisplay();
-			break;
-	
-		case 'o':
-		case 'O': {
-			if (spotlight_on) {
-				glDisable(GL_LIGHT1);
-				spotlight_on = 0;
-			} else {
-				glEnable(GL_LIGHT1);
-				spotlight_on = 1;
-			}
-			glutPostRedisplay();
-			break;
-		}
-	
 		case 27: /* ESC key */
 			exit(0);
 			break;
@@ -298,7 +320,6 @@ void keyboard(unsigned char key, int x, int y)
 		default:
 			break;
 	}
-	printf("spotlight cutoff: %2.0f\n", spotlight_cutoff);
 }
 
 /* 
@@ -309,7 +330,7 @@ int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize (600, 600); 
+	glutInitWindowSize (600, 400); 
 	glutInitWindowPosition (100, 100);
 	glutCreateWindow (argv[0]);
 	init ();
