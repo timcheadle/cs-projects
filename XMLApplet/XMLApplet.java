9ace6f355e5a03687d12a5e80fdb819d5505c79f
session <session>
2003-02-20 20:30:57 +0000

    Added code to iterate over hashes

--- XMLApplet/XMLApplet.java
+++ XMLApplet/XMLApplet.java
@@ -7,6 +7,7 @@
 package XMLApplet;
 
 import XMLApplet.Parser;
+import java.util.*;
 
 /**
  * @author session
@@ -16,5 +17,15 @@ public class XMLApplet {
 	public static void main(String[] args) {
 		Parser p = new Parser();
 		p.parse(args[0]);
+		
+		HashMap ageHash = p.getGPAs();
+		System.out.println("hash: " + ageHash);
+		Set ages = ageHash.keySet();
+		for (Iterator i = ages.iterator(); i.hasNext(); ) {
+			String key = i.next().toString();
+			String value = ageHash.get(key).toString();
+			System.out.println("  key: " + key);
+			System.out.println("value: " + value);
+		}
 	}
 }
