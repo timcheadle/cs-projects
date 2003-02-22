7694c5c2e23ef98262abbbfc5c214f490e532034
session <session>
2003-02-22 04:50:25 +0000

    finished unstuff(); done with project DLC1.

--- stuff.cpp
+++ stuff.cpp
@@ -20,76 +20,38 @@ void stack::zero_bits(bit_frame* zeros)
 //
 void stack::stuff(bit_frame* stuffed_frame,bit_frame* unstuffed_frame)
 {
-//
-// bit stuffing/unstuffing function for DLC in Network Workbench
-//
-// Algorithm for DLC 1: stuffing a bit frame
-// -----------------------------------------
-//
-// Copy the first eight bits to the output string without stuffing;
-// Scan the data bits from left to right for length minus sixteen,
-// {
-//   copy from input to output,
-//   {		
-//     maintain an index, starting at 8, in the input bit array; 
-//     another index, starting at 8, in the output array;
-//     and a counter, starting at zero;
-//     when copying a bit,
-//     { 
-//       move the input bit to output;
-//       if it is a zero bit, set the counter to zero;
-//       if it is a one bit, add one to the counter;
-//       if the counter value is five,
-//       {
-//         insert a zero in the output;
-//         zero the counter;
-//         add one to the output index;
-//       }
-//       add one to input index;
-//       add one to output index;
-//     }
-//   }
-// }
-// Copy the last eight bits without stuffing.
-//
-// NOTE: this version only copies the frame, student must change to 
-// add stuffing
-//
-
 	zero_bits(stuffed_frame);
 	int i_bitpos;
 	int o_bitpos;
 	int i;
-	int onecount = 0;
+	int one_count = 0;
 
 	// find length by converting the byte starting at bit 8
 	int unstuffed_length = 8*bittobyte(8,unstuffed_frame->frame_bits);
  
-	// Copy the first 8 bits into the output string
-	// since we know it's the beginning of a frame
+	// Copy the first 8 bits into the output frame
+	// since we know it's the beginning of the input frame
 	for (i_bitpos=0; i_bitpos < 8; i_bitpos++) {
 		stuffed_frame->frame_bits[i_bitpos] = unstuffed_frame->frame_bits[i_bitpos];
 	}
 
-	showbits(unstuffed_frame->frame_bits, unstuffed_length);
-	
 	// Now look for a string of five 1's in between the
 	// start and end bitstrings of the frame
 	for (i_bitpos=8, o_bitpos=8; i_bitpos < (unstuffed_length-8); i_bitpos++, o_bitpos++) {
 		bit current_bit = unstuffed_frame->frame_bits[i_bitpos];
 		if (current_bit == 0) {
 			stuffed_frame->frame_bits[o_bitpos] = current_bit;
-			onecount = 0;
+			one_count = 0;
 		} else  {
 			// If we found a 1, increment the counter and check to
 			// see if that's the fifth 1 we've see...
 			stuffed_frame->frame_bits[o_bitpos] = current_bit;
-			onecount++;
-			if (onecount == 5) {
+			one_count++;
+			if (one_count == 5) {
 				// If it is, then insert a 0 and reset the counter
-				stuffed_frame->frame_bits[o_bitpos] = 0;
 				o_bitpos++;
-				onecount = 0;
+				stuffed_frame->frame_bits[o_bitpos] = 0;
+				one_count = 0;
 			}
 		}
 	}
@@ -110,15 +72,52 @@ void stack::stuff(bit_frame* stuffed_frame,bit_frame* unstuffed_frame)
 //
 void stack::unstuff(bit_frame* unstuffed_frame,bit_frame* stuffed_frame)
 {
-  int bitpos;
-//
-// Algorithm for Unstuffing
-// ------------------------
-// Left as an exercise for the student (it is very much like stuffing)
-//
-// NOTE: this version only copies the frame, student must change to
-// add stuffing
-//
-  for(bitpos=0; bitpos<MAX_BIT_FRAME_SIZE; ++bitpos)
-    unstuffed_frame->frame_bits[bitpos] = stuffed_frame->frame_bits[bitpos];
+	zero_bits(unstuffed_frame);
+	int one_count = 0;
+	int i_bitpos = 0;
+	int o_bitpos = 0;
+	bool end_frame = false;
+	
+	// Copy the first 8 bits into the output frame
+	// since we know it's the beginning of the input frame
+	for (i_bitpos=0, o_bitpos=0; i_bitpos < 8; i_bitpos++, o_bitpos++) {
+		unstuffed_frame->frame_bits[o_bitpos] = stuffed_frame->frame_bits[i_bitpos];
+	}
+
+	// Now cycle through the input frame, looking for the flag 01111110
+	while (!end_frame && i_bitpos < MAX_BIT_FRAME_SIZE) {
+		bit current_bit = stuffed_frame->frame_bits[i_bitpos];
+
+		// If the current bit is a 0, copy it to the
+		// unstuffed frame and reset the 1 counter
+		if (current_bit == 0) {
+			unstuffed_frame->frame_bits[o_bitpos] = current_bit;
+			one_count = 0;
+		} else {
+			// If we found a 1, copy it over and increment the counter.
+			unstuffed_frame->frame_bits[o_bitpos] = current_bit;
+			one_count++;
+			
+			// If the counter is five, check the next bit (if it doesn't
+			// exceed the maximum frame length) to see what we should do next
+			if (one_count == 5 && (i_bitpos+1) < MAX_BIT_FRAME_SIZE) {
+				bit next_bit = stuffed_frame->frame_bits[i_bitpos+1];
+				
+				// If the next bit is a zero, ignore it and
+				// increment the input bit index
+				if (next_bit == 0) {
+					i_bitpos++;
+					one_count = 0;
+				} else {
+					// Otherwise, we've found six 1's and that means we're
+					// at the end of the frame.  Copy over a 1 and then a tailing 0
+					unstuffed_frame->frame_bits[++o_bitpos] = next_bit;
+					unstuffed_frame->frame_bits[++o_bitpos] = 0;
+					end_frame = true;
+				}
+			}
+		}
+		i_bitpos++;
+		o_bitpos++;
+	}
 }
