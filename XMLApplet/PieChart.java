2b518e574bda9ff5cb1f04b5aa741da04ed5b99c
session <session>
2003-03-05 20:05:31 +0000

    Added documentation for PieChart

--- XMLApplet/PieChart.java
+++ XMLApplet/PieChart.java
@@ -1,24 +1,42 @@
-/*
- * Created on Mar 5, 2003
- *
- * To change this generated comment go to 
- * Window>Preferences>Java>Code Generation>Code Template
- */
 package XMLApplet;
 
 import java.util.*;
 import java.awt.*;
 
 /**
- * @author tcheadle
+ * PieChart allows the simple creation of pie charts using AWT.  It
+ * gets the data to chart from a {@link HashMap}, and then all you need to tell
+ * it are the pixel locations where you want to plot the chart and it's legend.
+ * <p>
+ * It changes the {@link Color} of each pie slice according to the following order:
+ * <ol>
+ * <li><code>Color.green</code>
+ * <li><code>Color.yellow</code>
+ * <li><code>Color.red</code>
+ * <li><code>Color.orange</code>
+ * <li><code>Color.cyan</code>
+ * <li><code>Color.pink</code>
+ * <li><code>Color.magenta</code>
+ * <li><code>Color.lightGray</code>
+ * <li><code>Color.white</code>
+ * </ol>
+ * <p>
+ * If the number of slices exceeds the number of colors, the list cycles to the beginning.
+ * 
+ * @author Tim Cheadle
  */
 public class PieChart {
 	private HashMap data;
 	
 	private Color colors[] = new Color[9];
 	
-	public PieChart(HashMap d) {
-		data = d;
+	/**
+	 * Initializes the list of available slice colors and stores the series to chart.
+	 *   
+	 * @param series Data to chart
+	 */
+	public PieChart(HashMap series) {
+		data = series;
 		
 		colors[0] = Color.green;
 		colors[1] = Color.yellow;
@@ -31,6 +49,19 @@ public class PieChart {
 		colors[8] = Color.white;
 	}
 	
+	/**
+	 * Draws the pie chart and it's legend at the given coordinates using the given
+	 * width and height.
+	 * 
+	 * @param g       {@link Graphics} object to draw to
+	 * @param x       X coordinate of the middle of the pie chart circle
+	 * @param y       Y coordinate of the middle of the pie chart circle 
+	 * @param width   Width of the pie chart circle 
+	 * @param height  Height of the pie chart circle
+	 * @param legendX X coordinate of the top-left corner of the legend rectangle
+	 * @param legendY Y coordinate of the top-left corner of the legend rectangle
+	 * @see   Graphics
+	 */
 	public void draw(Graphics g, int x, int y, int width, int height, int legendX, int legendY) {
 		// First we have to calculate the total of all the values in the hash
 		// (which we're assuming are Integers)
