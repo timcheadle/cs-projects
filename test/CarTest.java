a0009a812d2d4428aaecabada2e95a08684eef55
session <session>
2003-04-13 06:58:21 +0000

    Added documentation and Exception testing in testCompareTo()

--- test/CarTest.java
+++ test/CarTest.java
@@ -16,6 +16,10 @@ public class CarTest extends TestCase {
 		super(name);
 	}
 
+	/**
+	 * Tests Car.compareTo() to make sure that comparisons are not performed on non-<code>Car</code>
+	 * classes, and ensure that the correct ordering of attributes is taking place.
+	 */
 	public void testCompareTo() {
 		Car a = new Car(
 			"Mercedes-Benz",
@@ -36,9 +40,26 @@ public class CarTest extends TestCase {
 			200,
 			4
 		);
-		 
+		
+		// Try to compare to a non-Car object
+		try {
+			a.compareTo(new Integer(2));
+		} catch (Exception e) {
+			assertTrue("Compare to non-car object throws ClassCastException", e instanceof ClassCastException);
+		}
+		
+		// Try to compare to a null object
+		try {
+			a.compareTo(null);
+		} catch (Exception e) {
+			assertTrue("Compare to null throws NullPointerException", e instanceof NullPointerException);
+		}
+		
+		// Compare an obejct to itself
 		assertTrue("Car should be the same as itself", a.compareTo(a) == 0);
-		assertTrue("Makes should descend", a.compareTo(b) > 0);
+		
+		// Make sure attributes are being ordered correctly
+		assertTrue("Makes of cars should descend (A-Z alphabetically)", a.compareTo(b) > 0);
 	}
 	
 	public static Test suite() {
