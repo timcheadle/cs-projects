5db0a1b95748f5a264a1428b9e2005626e293a1a
session <session>
2003-04-21 02:24:47 +0000

    Initial DDL for schema.

---
+++ project/hospitals.sql
@@ -0,0 +1,44 @@
+create table Laboratory_Technician (
+	Technician_ID numeric(8),
+	Laboratory_ID int
+);
+
+create table Laboratory ();
+
+create table Zip_Code ();
+
+create table Employee ();
+
+create table Staff ();
+
+create table Personal_Information ();
+
+create table Care_Center_Employee ();
+
+create table Care_Center ();
+
+create table Volunteer ();
+
+create table Registered_Nurse ();
+
+create table Nurse ();
+
+create table In_Patient ();
+
+create table Out_Patient ();
+
+create table Scheduled_Visit ();
+
+create table Physician ();
+
+create table Bed ();
+
+create table Patient ();
+
+create table Treatment ();
+
+create table Patient_Treatment ();
+
+create table Patient_Item ();
+
+create table Medical_Surgical_Item ();
